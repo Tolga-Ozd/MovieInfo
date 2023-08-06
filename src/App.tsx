1031f81e4ddf576b7814bfda7545e00855da2efc
Tolga ÖZDİLEK <ozdilektolga7140@gmail.com>
2023-08-06 14:33:24 +0300

    movie info

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import Home from "./components/Home" 
+import "./App.css"
+
+function App() {
+
+  return (
+    <div className="d-flex justify-content-center align-items-center vh-100" >
+     <Home />
+     
+    </div>
+  )
+}
+
+export default App
