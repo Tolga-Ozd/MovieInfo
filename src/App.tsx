f945224854fab9c9ff945604d482ff174d671bdf
Tolga ÖZDİLEK <ozdilektolga7140@gmail.com>
2023-08-10 14:41:18 +0300

    bg added

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import "./index.css"
 function App() {
   return (
     <>
-     <h1 className="d-flex m-4 justify-content-center display-1 shadow-lg text-danger">IMDB TOP "8"</h1>
+     <h1 className="d-flex m-4 bg-info rounded-2 justify-content-center display-1 shadow-lg text-danger">IMDB TOP "8"</h1>
       <div className="d-flex justify-content-center vertical-aling align-items-center " >
       
       <Home />
